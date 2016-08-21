4ed8693c99d13db25fffcd3f5902f1863cd8fd5e
jefferyrayrussell <jefferyrayrussell@gmail.com>
2016-08-21 12:48:33 -0700

    initial setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name="code-katas",
+    description="Code War Kata Challenges",
+    version='0.1.0',
+    author="Jeffery Russell",
+    author_email="jefferyrayrussell@gmail.com",
+    license='MIT',
+    py_modules=[],
+    package_dir={'': 'src'},
+    install_requires=[],
+    extras_require={'test': ['pytest', 'tox']},
+)
