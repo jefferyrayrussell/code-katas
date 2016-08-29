414f426ec75875ebdb6460127369dad54319ba30
jefferyrayrussell <jefferyrayrussell@gmail.com>
2016-08-29 00:28:37 -0700

    initial setup

---
+++ sort_cards.py
@@ -0,0 +1,15 @@
+# -*- coding: utf -8 -*-
+
+# Write a function sort_cards() that sorts a shuffled list of cards,
+# so that any given list of cards is sorted by rank, no matter the
+# starting collection.  All cards in the list are represented as strings,
+# so that sorted list of cards looks like this:
+# ['A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K'].
+
+
+# Assign each card above a priority value so that: A is 1, 2 is 2, 3 is 3,
+# 4 is 4, 5 is 5, 6 is 6, 7 is 7, 8 is 8, 9 is 9, T is 10, J is 11,
+# Q is 12, K is 13.
+
+# The numbered cards have their priority value equivalent to their face
+# value.
